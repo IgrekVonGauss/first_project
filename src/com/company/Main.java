06a62dac0fe39322416083c2086770689c49e330
Igor Savinov <igrek89@gmail.com>
2021-07-25 12:12:03 +0300

    warmup1 done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,28 +4,32 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String userInput;
-        do {
-            System.out.print("Input command: ");
-            userInput = in.nextLine();
-            String[] tokens = userInput.split(", ");
-            String command = tokens[0];
-            switch (command) {
-                case "exit":
-                    break;
-                case "front3":
-                    System.out.println(Warmup1.front3(tokens[1]));
-                    break;
-                default:
-                    System.out.println(command);
-                    System.err.println("Provided unknown command, please try again");
-                    break;
-            }
-        } while (!userInput.equalsIgnoreCase("exit"));
-        System.out.println("Program finished!");
-    }
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    String userInput;
+    do {
+      System.out.print("Input command: ");
+      userInput = in.nextLine();
+      String[] tokens = userInput.split(", ");
+      String command = tokens[0];
+      switch (command) {
+        case "exit";
+          break;
+        case "front3":
+          System.out.println(Warmup1.front3(tokens[1]));
+          break;
+        case "stringE":
+          Warmup1 war = new Warmup1();
+          System.out.println(war.stringE(tokens[1]));
+          break;
+        default:
+          System.out.println(command);
+          System.err.println("Provided unknown command, please try again");
+          break;
+      }
+    } while (!userInput.equalsIgnoreCase("exit"));
+    System.out.println("Program finished!");
+  }
 }
 
 
