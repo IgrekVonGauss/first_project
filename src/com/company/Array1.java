f9f07154f5606ee0cb0500d02def1eafe61bd38e
Igor Savinov <igrek89@gmail.com>
2021-07-26 10:11:32 +0300

    Arrays1 started

---
+++ src/com/company/Array1.java
@@ -0,0 +1,61 @@
+package com.company;
+
+public class Array1 {
+/*
+Given an array of ints, return true if 6 appears as either the first or last element in the array. The array will be length 1 or more.
+
+firstLast6([1, 2, 6]) → true
+firstLast6([6, 1, 2, 3]) → true
+firstLast6([13, 6, 1, 2, 3]) → false
+ */
+  public boolean firstLast6(int[] nums) {
+    return nums[0] == 6 || nums[nums.length-1] == 6;
+  }
+
+/*
+Given an array of ints, return true if the array is length 1 or more, and the first element and the last element are equal.
+
+
+sameFirstLast([1, 2, 3]) → false
+sameFirstLast([1, 2, 3, 1]) → true
+sameFirstLast([1, 2, 1]) → true
+ */
+  public boolean sameFirstLast(int[] nums) {
+    return nums.length >= 1 && nums[0] == nums[nums.length-1];
+  }
+
+/*
+Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+
+makePi() → [3, 1, 4]
+ */
+  public int[] makePi() {
+    int arrayLength = 3;
+    int[] res = new int[arrayLength];
+    int pi = (int)(Math.PI * Math.pow(10, arrayLength-1));
+    for (int i=0; i<arrayLength; i++)
+      res[i]=pi % (int)Math.pow(10, arrayLength-i);
+    return res;
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
